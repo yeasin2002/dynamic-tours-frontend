79cc76417e4f601cdca079004fe4316294710568
Sabbir Hossain <sabbir2always@gmail.com>
2024-02-08 20:42:36 +0600

    About section competed

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,14 +1,14 @@
 export default function About() {
   return (
     <>
-      <div className=" ">
-        <div className=" flex justify-between">
+      <div className=" py-6 ">
+        <div className=" flex justify-between flex-col lg:flex-row lg:gap-0 gap-10">
           <div className=" ">
             <h3 className=" p-4 text-lg text-center px-8 uppercase font-medium bg-offWhite rounded-3xl ">
               About Us
             </h3>
           </div>
-          <div className="  w-[60%] flex flex-col gap-5 ">
+          <div className=" w-full text-center lg:w-[60%] flex flex-col gap-5 ">
             <p className=" text-lg font-bold tracking-wide">
               Travelling Is Way To Explore New Places. Learn A Wonderfull Way To
               Explore New Places. Learn About Different Culture And Gain New
@@ -16,7 +16,7 @@ export default function About() {
               Gain New Exeperience
             </p>
 
-            <div className=" flex text-sm gap-8  text-textBlack">
+            <div className=" flex text-sm gap-8 flex-col md:flex-row text-textBlack">
               <div className=" bg-actionBlue p-1 rounded-full"></div>
               <p className="  ">
                 Travelling Is A Wonderfull Way To Explore New Places. Learn
@@ -33,6 +33,33 @@ export default function About() {
               </p>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className=" flex md:justify-center justify-between md:flex-row  gap-6 md:gap-12  py-10 flex-col flex-wrap">
+        <div className=" rounded-[50px] p-14 md:p-20 text-center bg-offWhite">
+          <h3 className=" text-8xl md:text-9xl font-thin text-actionBlue pb-6 tracking-wide">
+            10+
+          </h3>
+          <p className=" uppercase text-lg font-thin text-textBlack tracking-wide">
+            Year Experience
+          </p>
+        </div>
+        <div className=" rounded-[50px] p-14 md:p-20  text-center bg-offWhite">
+          <h3 className=" text-8xl md:text-9xl font-thin text-actionBlue pb-6 tracking-wide">
+            12K
+          </h3>
+          <p className=" uppercase text-lg font-thin text-textBlack tracking-wide">
+            Customer
+          </p>
+        </div>
+        <div className=" rounded-[50px] p-14 md:p-20  text-center bg-offWhite">
+          <h3 className="text-8xl md:text-9xl font-thin text-actionBlue pb-6 tracking-wide">
+            4.8
+          </h3>
+          <p className=" uppercase text-lg font-thin text-textBlack tracking-wide">
+            Overall Rating
+          </p>
         </div>
       </div>
     </>
