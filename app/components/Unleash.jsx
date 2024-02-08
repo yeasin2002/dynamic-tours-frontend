64d4aa280b0d06cd7c3755b367b469dfa525d657
Sabbir Hossain <sabbir2always@gmail.com>
2024-02-08 17:28:55 +0600

    unleash section complete

--- app/components/Unleash.jsx
+++ app/components/Unleash.jsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import kidImage from "@/public/kid.png";
-import imageforbg from "@/public/image/image1.jpg";
+import skyForButton from "@/public/sky.jpg";
+import { RiArrowRightLine } from "react-icons/ri";
+import Link from "next/link";
 
 export default function Unleash() {
   return (
     <>
       <div>
-        <div className="  flex flex-col gap- md:gap-12 xl:gap-20 lg:flex-row  items-center">
+        <div className="  flex flex-col gap- md:gap-12 xl:gap-20 lg:flex-row gap-8 items-center">
           <Image
             src={kidImage.src}
             width={450}
             height={670}
-            className=" object-cover w-[570px] h-[690px] rounded-[35px]"
+            className=" object-cover w-[570px] h-[600px] rounded-[35px]"
           />
 
           <div className=" flex text-center justify-center flex-col gap-7">
@@ -37,6 +39,32 @@ export default function Unleash() {
               </p>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="flex items-center flex-col md:flex-row gap-2 pt-4 md:pt-2">
+        <div className="  w-full md:w-2/6 flex  items-center p-7 justify-around  rounded-3xl bg-offWhite">
+          <h3 className=" text-actionBlue uppercase font-bold text-4xl lg:text-5xl">
+            20% OFF
+          </h3>
+          <p className=" text-textBlack text-lg font-medium">
+            Till 28 September, <br /> 2023
+          </p>
+        </div>
+        <div className=" flex-grow ">
+          <Link href={"#"}>
+            <div
+              style={{
+                backgroundImage: `url(${skyForButton.src})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              className="group text-3xl md:text-4xl text-center font-medium p-9  justify-center  rounded-3xl  flex gap-3 items-center"
+            >
+              <p>Book A Flight Now </p>
+              <RiArrowRightLine className="group-hover:translate-x-3 duration-300" />
+            </div>
+          </Link>
         </div>
       </div>
     </>
