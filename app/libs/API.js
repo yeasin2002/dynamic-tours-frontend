95015a5396ed8b104b3f68758f16da56e19d7690
Sabbir Hossain <sabbir2always@gmail.com>
2024-10-22 15:38:14 +0600

    add axios config to send access token on each request to the server

---
+++ app/libs/API.js
@@ -0,0 +1,24 @@
+"use server";
+import axios from "axios";
+import { auth } from "@/auth";
+
+const API = axios.create({
+  baseURL: "http://localhost:4000",
+});
+
+API.interceptors.request.use(
+  async (config) => {
+    const session = await auth();
+    const token = session?.accessToken;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default API;
--- app/libs/getTour.js
+++ app/libs/getTour.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+import API from "./API";
 
 export const getTour = async function (tourId) {
   try {
-    const tourData = await axios.get(
-      `http://localhost:4000/api/v1/tour/${tourId}`
-    );
+    const tourData = await API.get(`api/v1/tour/${tourId}`);
+    // const tourData = await axios.get(
+    //   `http://localhost:4000/api/v1/tour/${tourId}`
+    // );
+
     return tourData.data?.data;
   } catch (error) {
     console.log(error);
