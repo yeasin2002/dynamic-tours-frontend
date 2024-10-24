bc4c0620ee797a1176cf19630274914a3d78602d
Sabbir Hossain <sabbir2always@gmail.com>
2024-10-24 15:00:55 +0600

    resize part of login page

--- app/components/AuthForm/AuthForm.jsx
+++ app/components/AuthForm/AuthForm.jsx
@@ -11,13 +11,13 @@ export function AuthForm() {
   return (
     <>
       <div className="w-full h-screen flex items-center justify-center loginGradient">
-        <div className="flex h-full md:rounded-xl md:h-fit rounded-none  md:w-[90%] lg:w-[85%] xl:w-[70%] 2xl:w-[60%] shadow-xl  bg-white">
+        <div className="flex h-full md:rounded-xl md:h-fit rounded-none  md:w-[90%] lg:w-[85%] xl:w-[70%] 2xl:w-[65%] shadow-xl  bg-white">
           <div
-            className="w-1/2 min-h-[450px] hidden md:block bg-cover bg-center bg-no-repeat rounded-tl-xl rounded-bl-xl"
+            className="w-1/2 2xl:w-[55%] min-h-[450px] hidden md:block bg-cover bg-center bg-no-repeat rounded-tl-xl rounded-bl-xl"
             style={{ backgroundImage: `url('${authImage.src}')` }}
           ></div>
 
-          <div className=" w-full md:w-1/2 flex items-center ">
+          <div className=" w-full 2xl:w-[45%] md:w-1/2 flex items-center">
             {pathName === "/signup" && <SignUp />}
             {pathName === "/login" && <Login />}
           </div>
